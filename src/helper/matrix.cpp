82b352ee64d351c1aae758dc336eb1284feab314
daizihang <zander.dai@gmail.com>
2015-04-10 13:39:25 -0400

    SIMD

--- src/helper/matrix.cpp
+++ src/helper/matrix.cpp
@@ -2,14 +2,15 @@
 #include <algorithm>
 
 #include "matrix.h"
+#include <immintrin.h>
 
 using namespace std;
 
 #define SIMD_WIDTH 8
-#define MKL 1
+#define SIMD 1
 
 void outer (float *result, float *a, int dim_a, float *b, int dim_b) {
-	if (!MKL) {
+	if (!SIMD) {
 		for (int i=0; i<dim_a; ++i) {
 			for (int j=0; j<dim_b; ++j) {
 				// R_ij = a_i * b_j
@@ -23,7 +24,7 @@ void outer (float *result, float *a, int dim_a, float *b, int dim_b) {
 
 void dot (float *result, float *A, int dim1_A, int dim2_A, float *B, int dim1_B, int dim2_B) {
 	assert(dim2_A == dim1_B);
-	if (!MKL) {
+	if (!SIMD) {
 		int dim_inner = dim2_A;
 		for (int i=0; i<dim1_A; ++i) {
 			for (int j=0; j<dim2_B; ++j) {
@@ -40,7 +41,7 @@ void dot (float *result, float *A, int dim1_A, int dim2_A, float *B, int dim1_B,
 
 void trans_dot (float *result, float *A, int dim1_A, int dim2_A, float *B, int dim1_B, int dim2_B) {
 	assert(dim1_A == dim1_B);
-	if (!MKL) {
+	if (!SIMD) {
 		int dim_inner = dim1_A;
 		for (int i=0; i<dim2_A; ++i) {
 			for (int j=0; j<dim2_B; ++j) {
@@ -56,22 +57,22 @@ void trans_dot (float *result, float *A, int dim1_A, int dim2_A, float *B, int d
 }
 
 void elem_mul (float *result, float *a, float *b, int dim) {	
-	if (1) {
+	if (!SIMD) {
 		for (int i=0; i<dim; ++i) {
 			// R_i += a_i * b_i
 			result[i] += a[i] * b[i];
 		}
 	} else {
-		// __m256 vec_a, vec_b, vec_res;
-		// for (int i=0; i<dim; i+=SIMD_WIDTH) {
-		// 	vec_a = _mm256_loadu_ps(a + i);
-		// 	vec_b = _mm256_loadu_ps(b + i);
-		// 	vec_res = _mm256_loadu_ps(result + i);
+		__m256 vec_a, vec_b, vec_res;
+		for (int i=0; i<dim; i+=SIMD_WIDTH) {
+			vec_a = _mm256_loadu_ps(a + i);
+			vec_b = _mm256_loadu_ps(b + i);
+			vec_res = _mm256_loadu_ps(result + i);
 
-		// 	_mm256_mul_ps(vec_a, vec_b);
-		// 	_mm256_add_ps(vec_res, vec_a);
-		// 	_mm256_store_ps(result + i, vec_res);
-		// }		
+			_mm256_mul_ps(vec_a, vec_b);
+			_mm256_add_ps(vec_res, vec_a);
+			_mm256_storeu_ps(result + i, vec_res);
+		}
 	}
 }
 
