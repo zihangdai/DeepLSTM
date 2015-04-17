ce8f852fd7894372afcdd8e1d680951db07d9690
daizihang <zander.dai@gmail.com>
2015-04-16 23:43:37 -0400

    test translator

--- src/translator.cpp
+++ src/translator.cpp
@@ -17,16 +17,18 @@ int main() {
     // float data[10] = {1.f, 2.f, 2.f, 4.f, 3.f, 6.f, 4.f, 8.f, 5.f, 10.f};
     // float label[10] = {18.f, 9.f, 16.f, 8.f, 14.f, 7.f, 12.f, 6.f, 10.f, 5.f};
 
-    int inputSeqLen = confReader->getInt("max_sequence_length");
+    int dataSeqLen = confReader->getInt("data_sequence_length");
+    int targetSeqLen = confReader->getInt("target_sequence_length");
     int dimIn = confReader->getInt("data_size");
     int dimOut = confReader->getInt("target_size");
 
-    float *data = new float[dimIn * inputSeqLen];
-    float *label = new float[dimOut * inputSeqLen];
-    for (int i=0; i<inputSeqLen; ++i) {
+    float *data = new float[dimIn * dataSeqLen];
+    float *label = new float[dimOut * targetSeqLen];
+    for (int i=0; i<dataSeqLen; ++i) {
         for (int j=0; j<dimIn; ++j) {
             data[i*dimIn+j] = 1.f * (float(rand()) / float(RAND_MAX) + 0.5);
         }
+    for (int i=0; i<targetSeqLen; ++i) {
         for (int j=0; j<dimOut; ++j) {
             label[i*dimOut+j] = 5.f * (float(rand()) / float(RAND_MAX) + 0.5);
         }
