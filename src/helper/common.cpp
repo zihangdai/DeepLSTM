283bddb5865ec9a9cf3c825192ca6601e3e52233
daizihang <zander.dai@gmail.com>
2015-04-20 23:23:16 -0400

    nonlinearity

--- src/helper/common.cpp
+++ src/helper/common.cpp
@@ -16,7 +16,9 @@ __m256 _ps256_cephes_exp_p5 = _mm256_set1_ps(5.0000001201E-1);
 
 __m256 _ps256_0p5 = _mm256_set1_ps(0.5f);
 
+
 __m256i _pi32_256_0x7f = _mm256_set1_epi32(0x7f);
+__m128i _pi32_0x7f = _mm_set1_epi32(0x7f);
 
 __m256 exp256_ps(__m256 x) {
   __m256 tmp = _mm256_setzero_ps(), fx;  
@@ -63,13 +65,34 @@ __m256 exp256_ps(__m256 x) {
   y = _mm256_add_ps(y, x);
   y = _mm256_add_ps(y, one);
 
-  /* build 2^n */
-  __m256i imm0;
-  imm0 = _mm256_cvttps_epi32(fx);
-  // another two AVX2 instructions
-  imm0 = _mm256_add_epi32(imm0, _pi32_256_0x7f);
-  imm0 = _mm256_slli_epi32(imm0, 23);
-  __m256 pow2n = _mm256_castsi256_ps(imm0);
+  // AVX2 Version
+  // /* build 2^n */
+  // __m256i imm0;
+  // imm0 = _mm256_cvttps_epi32(fx);
+  // // another two AVX2 instructions
+  // imm0 = _mm256_add_epi32(imm0, _pi32_256_0x7f);
+  // imm0 = _mm256_slli_epi32(imm0, 23);
+  // __m256 pow2n = _mm256_castsi256_ps(imm0);
+
+  // SSE2 version
+  __m128i emm0, emm1;
+  __m128 fx0, fx1;
+  fx0 = _mm256_extractf128_ps(fx, 0);
+  fx1 = _mm256_extractf128_ps(fx, 1);
+  // first 4
+  emm0 = _mm_cvttps_epi32(fx0);
+  emm0 = _mm_add_epi32(emm0, _pi32_0x7f);
+  emm0 = _mm_slli_epi32(emm0, 23);
+  __m128 pow2n0 = _mm_castsi128_ps(emm0);
+  y0 = 
+  // second 4
+  emm1 = _mm_cvttps_epi32(fx1);
+  emm1 = _mm_add_epi32(emm1, _pi32_0x7f);
+  emm1 = _mm_slli_epi32(emm1, 23);
+  __m128 pow2n1 = _mm_castsi128_ps(emm1);
+  // pow2n
+  __m256 pow2n = _mm256_insertf128_ps(pow2n, pow2n0, 0);
+  pow2n = _mm256_insertf128_ps(pow2n, pow2n1, 1);    
   y = _mm256_mul_ps(y, pow2n);
   return y;
 }
