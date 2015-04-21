2d10608489112b1ac46645dcb395eb981885e80c
daizihang <zander.dai@gmail.com>
2015-04-21 04:04:17 -0400

    gradient check

--- src/main.cpp
+++ src/main.cpp
@@ -34,14 +34,10 @@ int main(int argc, char* argv[]) {
             data[i*dimIn+j] = i*dimIn+j;
         }
         for (int j=0; j<dimOut; ++j) {
-            label[i*dimOut+j] = dimOut * inputSeqLen - i*dimIn+j;
+            label[i*dimOut+j] = dimOut * inputSeqLen - (i*dimIn+j);
         }
-    }   
-    for (int i=0; i<20; ++i) {
-        printf("%f\t", params[i]);
-    }
-    printf("\n");
-        
+    }       
+
     float error = net->computeGrad(grad, params, data, label, 1);
 
     for (int i=0; i<20; ++i) {
